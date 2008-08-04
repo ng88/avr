691fba5b03c75331e1dcaae71e7659caf03a4053
ng <nicolas@nghs.fr>
2008-08-04 10:34:58 +0000

    git-svn-id: file:///Users/ng/svn-local/to_share/avr@27 66d6013a-baa1-4d00-a207-16d12b192b5b

--- kb/kb.h
+++ kb/kb.h
@@ -15,6 +15,18 @@
 #define ISC0_FALLING_EDGE 2
 #define ISC0_RISING_EDGE 3
 
+enum
+{
+    CMD_SET_LED_STATUS = 0xed,
+    CMD_RESET = 0xff,
+    CMD_ECHO = 0xee,
+    CMD_SET_SC_SET = 0xf0,
+    CMD_SET_REPEAT_RATE = 0xf3,
+    CMD_KEYBOARD_ENABLE = 0xf4,
+    CMD_KEYBOARD_DISABLE = 0xf5,
+    CMD_RESENT = fe,
+};
+
 void kb_init(void);
 
 ISR(INT0_vect);
